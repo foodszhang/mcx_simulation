62dec9460ec72110b96269d7be876607b002d329
foods <foods@foods.>
2026-04-09 10:37:24 +0800

    add

--- legacy/FMT_DE_mouse/FMT_DE_forward/get_surface_energy.m
+++
@@ -1,6 +0,0 @@
-function surf_fym=get_surface_energy(sp3result,elementSurface,num_esource)
-global  nodes
-surface_node_index=unique(elementSurface(:,2:end));
-surf_fym =zeros(size(nodes,1),num_esource);
-surf_fym(surface_node_index,:) = sp3result(surface_node_index,:);
-  
